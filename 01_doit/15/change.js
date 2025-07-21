24838be8989df04727db4072a6095ed0e2ee2326
kozang <sinwon0214@gmail.com>
2025-07-21 16:00:40 +0900

    프로젝트 구조 및 UI/UX 개선, 모바일 푸터 고정 등 주요 변경사항 반영

---
+++ 01_doit/15/change.js
@@ -0,0 +1,5 @@
+/* = : 좌항에 우항을 할당 */
+let heading = document.getElementById('heading');
+heading.onclick = function () {
+    heading.style.color = 'red';
+}
---
+++ 01_doit/16/primitive.js
@@ -0,0 +1,7 @@
+/* === primitive === */
+/* 1. number */
+let num1 = 1;
+let num2 = 2;
+/* console.log(typeof num1); */
+let sum = num1 + num2; //연산자
+console.log('sum:' + sum + '///// sum type:' + typeof sum);
